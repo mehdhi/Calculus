ce9bea4d7217f827edbeb086c11a6db2c6e73b99
Mehdhi Nawaz <mehdhi@users.noreply.github.com>
2017-05-21 19:45:15 +0530

    fucn update

--- Calculus/CalculatorEngine.swift
+++ Calculus/CalculatorEngine.swift
@@ -19,7 +19,7 @@ class CalculatorEngine :NSObject
         self.operandStack.append( value )
     }
     
-    func operate( operation: String ) -> Double
+    func operate( operation: String ) throws -> Double
     {
         switch operation
         {
@@ -31,7 +31,7 @@ class CalculatorEngine :NSObject
         case "÷":
             if operandStack.count > 1
             {
-                return self.operandStack.removeLast() / self.operandStack.removeLast()
+                return try self.divide(divisor: self.operandStack.removeLast(), value: self.operandStack.removeLast())
             }
         case "+":
             if operandStack.count > 1
@@ -43,6 +43,53 @@ class CalculatorEngine :NSObject
             {
                 return self.operandStack.removeLast() - self.operandStack.removeLast()
             }
+        case "√":
+            if operandStack.count >= 1
+            {
+                return self.operandStack.removeLast().squareRoot()
+            }
+        case "x2":
+            if operandStack.count >= 1
+            {
+                let x = self.operandStack.removeLast()
+                return   x * x
+            }
+        case "1/x":
+            if operandStack.count >= 1
+            {
+                return try self.divide(divisor: 1, value: self.operandStack.removeLast())
+            }
+        case "sin":
+            if operandStack.count >= 1
+            {
+                return sin( self.operandStack.removeLast() )
+            }
+        case "cos":
+            if operandStack.count >= 1
+            {
+                return cos( self.operandStack.removeLast() )
+            }
+        case "tan":
+            if operandStack.count >= 1
+            {
+                return tan( self.operandStack.removeLast() )
+            }
+        case "asin":
+            if operandStack.count >= 1
+            {
+                return asin( self.operandStack.removeLast() )
+            }
+        case "acos":
+            if operandStack.count >= 1
+            {
+                return acos( self.operandStack.removeLast() )
+            }
+        case "atan":
+            if operandStack.count >= 1
+            {
+                return atan( self.operandStack.removeLast() )
+            }
+            
         default: break
             
         }
@@ -50,6 +97,16 @@ class CalculatorEngine :NSObject
         return 0.0
     }
     
+    private func divide( divisor :Double , value :Double ) throws -> Double {
+        if divisor != 0 {
+            return value / divisor
+        }
+        throw NSError(domain:"Cannot Divide by 0", code: 1009, userInfo:nil)
+    }
     
+    func clearStack()
+    {
+        operandStack.removeAll();
+    }
     
 }
--- Calculus/ViewController.swift
+++ Calculus/ViewController.swift
@@ -28,15 +28,38 @@ class ViewController: UIViewController {
     
     @IBAction func digitPressed(_ sender: UIButton) {
         
-        let digit = sender.currentTitle!
+        var digit = sender.currentTitle!
         print("digit pressed = "  + digit )
+        if digit == "." && (labelDisplay.text?.contains("."))! {
+            return
+        }
         if userHasStartedTyping {
             labelDisplay.text = labelDisplay.text! + "\(digit)"
         } else {
+            if digit == "." {
+                digit = "\(0)."
+            }
             labelDisplay.text = digit
             userHasStartedTyping = true;
         }
         
+    }
+    
+    @IBAction func deleteLastCharacter() {
+        if (!(labelDisplay.text?.isEmpty)!) {
+            let endIndex = labelDisplay.text?.index((labelDisplay.text?.endIndex)!, offsetBy: -1)
+            labelDisplay.text = labelDisplay.text?.substring(to: endIndex! )
+            if (labelDisplay.text?.isEmpty)! {
+                labelDisplay.text = "\(0)"
+                userHasStartedTyping = false
+            }
+        }
+    }
+    
+    @IBAction func clearAll() {
+        labelDisplay.text = "\(0)";
+        self.calcEngine?.clearStack()
+        userHasStartedTyping = false
     }
     
     var displayValue : Double {
@@ -48,7 +71,7 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction func operation(_ sender: UIButton) {
+    @IBAction func operation(_ sender: UIButton) {	
         
         let operation = sender.currentTitle!
         
@@ -56,22 +79,35 @@ class ViewController: UIViewController {
             enter()
         }
         
-        self.displayValue = (self.calcEngine?.operate( operation: operation ))!
+        do {
+            try self.displayValue = ( self.calcEngine?.operate( operation: operation ))!
+        } catch {
+            self.labelDisplay.text = "Error"
+            userHasStartedTyping = false;
+            return
+        }
         
         enter()
         
         
     }
     
+    @IBAction func inverseValue() {
+        if (self.labelDisplay.text?.contains("-"))! {
+            self.labelDisplay.text?.remove(at: (self.labelDisplay.text?.startIndex)!)
+        } else {
+            self.labelDisplay.text = "-" + self.labelDisplay.text!
+        }
+    }
+    
     @IBAction func enter() {
         print( "Pressed Enter")
         self.userHasStartedTyping = false;
-        self.calcEngine!.operandStack.append( displayValue )
+        self.calcEngine!.updateStackWithValue(value: displayValue )
         
         print( "Operand Stack on engine = \(self.calcEngine!.operandStack)")
         
     }
-    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
